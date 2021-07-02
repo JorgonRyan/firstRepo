b26337bd473776eab86f09bd1f5301b349edcde2
JorgonRyan <jorgon_ryan@hcl.com>
2021-07-02 15:53:28 -0500

    Finished Collection Exercises, Started Collection Tests - Need to find suitable collection testing package

--- javaexercises/src/main/java/com/hcl/javaexercises/collections/CollectionsObj.java
+++ javaexercises/src/main/java/com/hcl/javaexercises/collections/CollectionsObj.java
@@ -1,5 +1,69 @@
 package com.hcl.javaexercises.collections;
 
-class CollectionsObj {
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeMap;
+import java.util.TreeSet;
 
+class CollectionsObj {
+	List<Object> list = new ArrayList<Object>();
+
+	void printCollection(String[] colors) {
+		for (int i = 0; i < colors.length; i++) {
+			list.add(colors[i]);
+		}
+		System.out.println(list);
+	}
+
+	void iterateList(ArrayList<Object> iterateThrough) {
+		while (iterateThrough.iterator().hasNext()) {
+			System.out.println(iterateThrough.iterator().next());
+		}
+	}
+
+	List<Object> reverseOrder(List<Object> reverseThis) {
+		List<Object> reversed = reverseThis.subList(0, reverseThis.size());
+		Collections.sort(reversed, Collections.reverseOrder());
+		return reversed;
+	}
+
+	Set<Object> similarSet(Set<Object> set1, Set<Object> set2) {
+		Set<Object> similarities = new HashSet<Object>();
+		Iterator<Object> set1Value = set1.iterator(), set2Value = set2.iterator();
+		while (set1Value.hasNext() && set2Value.hasNext()) {
+			if (set1Value.next() == set2Value.next()) {
+				similarities.add(set2Value.next());
+			}
+		}
+		return similarities;
+	}
+
+	Set<Integer> greaterThanSeven(TreeSet<Integer> integerTree) {
+		Set<Integer> lowerVals = new HashSet<Integer>();
+		int currentMark = integerTree.lower(7);
+		lowerVals.add(currentMark);
+		while (integerTree.lower(currentMark) != null) {
+			currentMark = integerTree.lower(currentMark);
+			lowerVals.add(currentMark);
+		}
+		return lowerVals;
+	}
+
+	int numberOfMappings(Map<Object, Object> map) {
+		return map.size();
+	}
+
+	void mapSetView(Map<Object, Object> map) {
+		System.out.println(map.keySet());
+	}
+
+	Map<Object, Object> sortATree(TreeMap<Object, Object> treeMap) {
+		treeMap.comparator();
+		return treeMap;
+	}
 }
--- javaexercises/src/test/java/com/hcl/javaexercises/collections/CollectionsObjTest.java
+++ javaexercises/src/test/java/com/hcl/javaexercises/collections/CollectionsObjTest.java
@@ -1,5 +1,58 @@
 package com.hcl.javaexercises.collections;
 
-public class CollectionsObjTest {
+import static org.junit.Assert.assertEquals;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class CollectionsObjTest {
+	CollectionsObj collector = new CollectionsObj();
+
+	private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+	private final PrintStream originalOut = System.out;
+
+	@Before
+	public void setUpStream() {
+		System.setOut(new PrintStream(outContent));
+	}
+
+	@Test
+	public void printCollectionTest() {
+		String[] colors = { "red", "white", "blue" };
+		collector.printCollection(colors);
+		assertEquals("[red, white, blue]", outContent.toString().trim());
+	}
+
+	public void iterateListTest() {
+		ArrayList<Object> intList = new ArrayList<Object>();
+		intList.add(1);
+		intList.add(2);
+		intList.add(3);
+		intList.add(4);
+		collector.iterateList(intList);
+		assertEquals("[1, 2, 3, 4]", outContent.toString().trim());
+	}
+
+	public void reverseOrderTest() {
+		List<Object> reverseThis = new ArrayList<Object>();
+		reverseThis.add("hello");
+		reverseThis.add("I");
+		reverseThis.add("Like");
+		reverseThis.add("To");
+		reverseThis.add("Program");
+		List<Object> reversed = new ArrayList<Object>();
+		// Need to find asserts for Collection objects!
+		// assertArrayEquals(reversed, collector.reverseOrder(reverseThis));
+	}
+
+	@After
+	public void restoreStream() {
+		System.setOut(originalOut);
+	}
 }
